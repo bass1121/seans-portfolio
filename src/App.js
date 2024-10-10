76c811286816604b560fbaa41c243351319941a8
bass1121 <seanbass99@gmail.com>
2024-10-10 18:09:57 -0500

    set up App.js, imported moment library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import moment from "moment";
+import './styles/App.css';
+
+export default class App extends Component {
+  render() {
+      return (
+        <div className="app">
+        <h1>Sean Bass's Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+      </div>
+    );
+  }
 }
-
-export default App;

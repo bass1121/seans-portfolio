db83078a3fad2e0d06bff2fa7f0cce474a64c1d0
bass1121 <seanbass99@gmail.com>
2024-10-11 16:47:50 -0500

    added constructor to portfolio container and added to portfolioItems object

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-
 import React, { Component } from 'react';
 import moment from "moment";
 import './styles/App.css';
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
+
   render() {
       return (
         <div className="app">
-        <h1>Sean Bass's Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-      </div>
+          <h1>Sean Bass's Portfolio</h1>
+          <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+          <PortfolioContainer />
+        </div>
     );
   }
 }
